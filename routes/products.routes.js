7915bdb6f912163e8b91c6e85ebf566ab4198eef
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-05-12 20:47:33 -0300

    feat: add Products.list

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const Product = require('../models/productModel');
 
 const productsRoutes = Router();
 
-productsRoutes.get('/', (req, res) => {
-  res.status(500).json({ message: 'rota incompleta' });
+productsRoutes.get('/', async (req, res) => {
+  const products = await Product.list();
+  res.status(500).json({ products });
 });
 
 module.exports = { productsRoutes };
