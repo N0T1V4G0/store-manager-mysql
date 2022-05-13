e8782a30f4866a835fde8aef87f6299e39b6fa64
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-05-12 21:41:08 -0300

    feat: add getByID in products controllers

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -4,3 +4,9 @@ exports.list = async (req, res) => {
   const products = await productsService.list();
   res.status(200).json(products);
 };
+
+exports.getByID = async (req, res) => {
+  const { id } = req.params;
+  const product = await productsService.getByID(id * 1);
+  res.status(200).json(product); 
+};
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -4,5 +4,6 @@ const productsControllers = require('../controllers/productsControllers');
 const productsRoutes = Router();
 
 productsRoutes.get('/', productsControllers.list);
+productsRoutes.get('/:id', productsControllers.getByID);
 
 module.exports = { productsRoutes };
