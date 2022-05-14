608eae932b87a537bd0dc01f5b428773c870a729
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-05-14 11:44:30 -0300

    add getByID sale service

--- services/salesService.js
+++ services/salesService.js
@@ -1,6 +1,15 @@
+const AppError = require('../errors/AppError');
 const Sale = require('../models/saleModel');
 
 exports.list = async () => {
   const sales = await Sale.list();
   return sales;
+};
+
+exports.getByID = async (id) => {
+  const sale = await Sale.getByID(id);
+  if (!sale) {
+    throw new AppError('Sale not found', 404);
+  }
+  return sale;
 };
