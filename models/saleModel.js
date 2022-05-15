dd4f31a0730bec78966a06976ef2fcf2f89c49c1
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-05-15 00:19:30 -0300

    fix: sale models calls correct table

--- models/saleModel.js
+++ models/saleModel.js
@@ -1,14 +1,14 @@
 const { connection } = require('./connection');
 
 exports.list = async () => {
-  const [data] = await connection.execute('SELECT * FROM StoreManager.sales;');
+  const [data] = await connection.execute('SELECT * FROM StoreManager.sales_products;');
 
   return data;
 };
 
 exports.getByID = async (id) => {
   const [data] = await connection.execute(
-    'SELECT * FROM StoreManager.sales WHERE id = ?',
+    'SELECT * FROM StoreManager.sales_products WHERE sale_id = ?',
     [id],
   );
 
