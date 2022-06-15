cad08f16e79bf08584f1e9e6f7e5da7516f9182f
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-06-14 21:29:24 -0300

    refactor: remove unnecessary curly braces

--- services/productsService.js
+++ services/productsService.js
@@ -1,33 +1,24 @@
 const AppError = require('../errors/AppError');
 const Product = require('../models/productModel');
 
-exports.list = async () => {
-  const products = await Product.list();
-  return products;
-};
+exports.list = async () => Product.list();
 
 exports.getByID = async (id) => {
   const product = await Product.getByID(id);
-  if (!product) {
-    throw new AppError('Product not found', 404);
-  }
+  if (!product) throw new AppError('Product not found', 404);
   return product;
 };
 
 exports.create = async ({ name, quantity }) => {
   const productAlreadyExists = await Product.getByName(name);
-  if (productAlreadyExists) {
-    throw new AppError('Product already exists', 409);
-  }
+  if (productAlreadyExists) throw new AppError('Product already exists', 409);
   const newProduct = await Product.create({ name, quantity });
   return newProduct;
 };
 
 exports.update = async ({ id, name, quantity }) => {
   const productAlreadyExists = await Product.getByID(id);
-  if (!productAlreadyExists) {
-    throw new AppError('Product not found', 404);
-  }
+  if (!productAlreadyExists) throw new AppError('Product not found', 404);
   await Product.update({ id, name, quantity });
   const updatedProduct = await Product.getByID(id);
   return updatedProduct;
@@ -35,8 +26,6 @@ exports.update = async ({ id, name, quantity }) => {
 
 exports.delete = async (id) => {
   const productAlreadyExists = await Product.getByID(id);
-  if (!productAlreadyExists) {
-    throw new AppError('Product not found', 404);
-  }
+  if (!productAlreadyExists) throw new AppError('Product not found', 404);
   await Product.delete(id);
 };
